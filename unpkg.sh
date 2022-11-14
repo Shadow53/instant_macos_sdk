8339546e11e8c412edf977f3bb429a10045192ae
Michael Bryant <shadow53@shadow53.com>
2022-11-13 17:36:15 -0800

    don't use non-portable cpio flag

--- unpkg.sh
+++ unpkg.sh
@@ -5,17 +5,19 @@ cd "$(dirname "$0")"/sdk
 echo "# Extracting the SDK"
 
 mkdir root
-# L.egacy?
-#pbzx CLTools_macOSLMOS_SDK.pkg | cpio -i -D root
-# N.ew?
-pbzx CLTools_macOSNMOS_SDK.pkg | cpio -i -D root
-# Tools
-pbzx CLTools_Executables.pkg | cpio -i -D root
-# SDK
-pbzx CLTools_macOS_SDK.pkg | cpio -i -D root
+cd root
 
-mkdir -p root/Contents/Developer/Platforms/MacOSX.platform/Developer
-mv root/Library/Developer/CommandLineTools/SDKs\
-	root/Contents/Developer/Platforms/MacOSX.platform/Developer
-cp -r root/Library/Developer/CommandLineTools/usr\
-	root/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX*.*.sdk
+# L.egacy?
+#pbzx CLTools_macOSLMOS_SDK.pkg | cpio -i root
+# N.ew?
+pbzx CLTools_macOSNMOS_SDK.pkg | cpio -i root
+# Tools
+pbzx CLTools_Executables.pkg | cpio -i root
+# SDK
+pbzx CLTools_macOS_SDK.pkg | cpio -i root
+
+mkdir -p Contents/Developer/Platforms/MacOSX.platform/Developer
+mv Library/Developer/CommandLineTools/SDKs\
+	Contents/Developer/Platforms/MacOSX.platform/Developer
+cp -r Library/Developer/CommandLineTools/usr\
+	Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX*.*.sdk
