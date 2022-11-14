f241e858b6d64a3221377872dacec8647c0e1a9e
Michael Bryant <shadow53@shadow53.com>
2022-11-14 00:37:46 -0800

    use correct path to pkg files

--- unpkg.sh
+++ unpkg.sh
@@ -10,13 +10,13 @@ mkdir root
 cd root
 
 # L.egacy?
-#pbzx CLTools_macOSLMOS_SDK.pkg | cpio -i
+#pbzx ../CLTools_macOSLMOS_SDK.pkg | cpio -i
 # N.ew?
-pbzx CLTools_macOSNMOS_SDK.pkg | cpio -i
+pbzx ../CLTools_macOSNMOS_SDK.pkg | cpio -i
 # Tools
-pbzx CLTools_Executables.pkg | cpio -i
+pbzx ../CLTools_Executables.pkg | cpio -i
 # SDK
-pbzx CLTools_macOS_SDK.pkg | cpio -i
+pbzx ../CLTools_macOS_SDK.pkg | cpio -i
 
 mkdir -p Contents/Developer/Platforms/MacOSX.platform/Developer
 mv Library/Developer/CommandLineTools/SDKs\
