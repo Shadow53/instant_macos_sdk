3a4960c6a6fc884c1007ce1a141234393b437176
Michael Bryant <shadow53@shadow53.com>
2022-11-13 20:31:29 -0800

    don't specify target dir for cpio

--- unpkg.sh
+++ unpkg.sh
@@ -8,13 +8,13 @@ mkdir root
 cd root
 
 # L.egacy?
-#pbzx CLTools_macOSLMOS_SDK.pkg | cpio -i root
+#pbzx CLTools_macOSLMOS_SDK.pkg | cpio -i
 # N.ew?
-pbzx CLTools_macOSNMOS_SDK.pkg | cpio -i root
+pbzx CLTools_macOSNMOS_SDK.pkg | cpio -i
 # Tools
-pbzx CLTools_Executables.pkg | cpio -i root
+pbzx CLTools_Executables.pkg | cpio -i
 # SDK
-pbzx CLTools_macOS_SDK.pkg | cpio -i root
+pbzx CLTools_macOS_SDK.pkg | cpio -i
 
 mkdir -p Contents/Developer/Platforms/MacOSX.platform/Developer
 mv Library/Developer/CommandLineTools/SDKs\
