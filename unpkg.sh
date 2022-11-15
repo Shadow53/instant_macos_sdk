793432e9f4c2a8e512b9c4a98e3d98a6801cd7f3
Michael Bryant <shadow53@shadow53.com>
2022-11-15 00:57:08 -0800

    more verbosity while unpacking/copying cpio files

--- unpkg.sh
+++ unpkg.sh
@@ -10,14 +10,14 @@ mkdir root
 cd root
 
 # L.egacy?
-#pbzx ../CLTools_macOSLMOS_SDK.pkg | cpio -i
+#pbzx ../CLTools_macOSLMOS_SDK.pkg | cpio -i -d -u -v
 # N.ew?
-pbzx ../CLTools_macOSNMOS_SDK.pkg | cpio -i
+pbzx ../CLTools_macOSNMOS_SDK.pkg | cpio -i -d -u -v
 # Tools
-pbzx ../CLTools_Executables.pkg | cpio -i
+pbzx ../CLTools_Executables.pkg | cpio -i -d -u -v
 # SDK
-pbzx ../CLTools_macOS_SDK.pkg | cpio -i
+pbzx ../CLTools_macOS_SDK.pkg | cpio -i -d -u -v
 
 mkdir -p Contents/Developer/Platforms/MacOSX.platform/Developer
 mv Library/Developer/CommandLineTools/SDKs Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
-cp -r Library/Developer/CommandLineTools/usr Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX*.*.sdk/usr
+cp -rfv Library/Developer/CommandLineTools/usr Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX*.*.sdk/usr
