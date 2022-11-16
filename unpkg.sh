0bb3b67df7215e12de8bd0fcfbb9bf9361df9d5b
Michael Bryant <shadow53@shadow53.com>
2022-11-15 23:21:37 -0800

    use cp -R for BSD compat, remove verbose flags

--- unpkg.sh
+++ unpkg.sh
@@ -6,18 +6,18 @@ cd "$(dirname "$0")"/sdk
 
 echo "# Extracting the SDK"
 
-mkdir root
+mkdir -p root
 cd root
 
 # L.egacy?
-#pbzx ../CLTools_macOSLMOS_SDK.pkg | cpio -i -d -u -v
+#pbzx ../CLTools_macOSLMOS_SDK.pkg | cpio -i -d -u
 # N.ew?
-pbzx ../CLTools_macOSNMOS_SDK.pkg | cpio -i -d -u -v
+pbzx ../CLTools_macOSNMOS_SDK.pkg | cpio -i -d -u
 # Tools
-pbzx ../CLTools_Executables.pkg | cpio -i -d -u -v
+pbzx ../CLTools_Executables.pkg | cpio -i -d -u
 # SDK
-pbzx ../CLTools_macOS_SDK.pkg | cpio -i -d -u -v
+pbzx ../CLTools_macOS_SDK.pkg | cpio -i -d -u
 
 mkdir -p Contents/Developer/Platforms/MacOSX.platform/Developer
 mv Library/Developer/CommandLineTools/SDKs Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs
-cp -rfv Library/Developer/CommandLineTools/usr Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX*.*.sdk/usr
+cp -Rf Library/Developer/CommandLineTools/usr Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX*.*.sdk/usr
